7dfa1fd53551a99fecce62e257f676ef2329680e
Richard Steinbrück <r.steinbrueck@pixel-combo.de>
2019-10-18 14:59:32 +0200

    solved the problem with passing through projectids

--- outputs.tf
+++ outputs.tf
@@ -3,25 +3,27 @@
 # ==================================================================
 
 output "email" {
-    value       = "${google_service_account.default.email}"
-    description = "The e-mail address of the service account."
+  value       = "${google_service_account.default.email}"
+  description = "The e-mail address of the service account."
 }
+
 output "name" {
-    value       = "${google_service_account.default.name}"
-    description = "The fully-qualified name of the service account."
+  value       = "${google_service_account.default.name}"
+  description = "The fully-qualified name of the service account."
 }
+
 output "unique_id" {
-    value       = "${google_service_account.default.unique_id}"
-    description = "The unique id of the service account."
+  value       = "${google_service_account.default.unique_id}"
+  description = "The unique id of the service account."
 }
 
 # ==================================================================
 # private key and decoded private key
 # ==================================================================
 output "private_key" {
-    value = "${google_service_account_key.default.private_key}"
+  value = "${google_service_account_key.default.private_key}"
 }
 
 output "decoded_private_key" {
-    value = "${base64decode(google_service_account_key.default.private_key)}"
+  value = "${base64decode(google_service_account_key.default.private_key)}"
 }
