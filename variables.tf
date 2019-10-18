b555a242999f01d0b0a77caecf2b4bd06377ed46
Richard Steinbrück <r.steinbrueck@pixel-combo.de>
2019-10-18 14:59:32 +0200

    solved the problem with passing through projectids

--- variables.tf
+++ variables.tf
@@ -1,13 +1,32 @@
-variable "account_id" {
-    description = "The service account ID. Changing this forces a new service account to be created."
-}
-variable "display_name" {
-    description = "The display name for the service account. Can be updated without creating a new resource."
-    default     = "Managed-by-Terraform"
-}
-variable "roles" {
-    type        = "list"
-    description = "The roles that will be granted to the account."
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# These parameters must be supplied when consuming this module.
+# ---------------------------------------------------------------------------------------------------------------------
 
-    default = []
+variable "project_id" {
+  description = "Project ID"
+  type        = "string"
 }
+
+variable "account_id" {
+  description = "The service account ID. Changing this forces a new service account to be created."
+  type        = "string"
+}
+
+variable "display_name" {
+  description = "The display name for the service account. Can be updated without creating a new resource."
+  default     = "Managed-by-Terraform"
+}
+
+variable "roles" {
+  type        = "list"
+  description = "The roles that will be granted to the account."
+
+  default = []
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
